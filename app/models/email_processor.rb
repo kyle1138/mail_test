b3d35b81bff8f14811dfa529731a92e7f6ed5412
Kyle Hogan <kylehogan@Kyles-Air.home>
2015-03-12 17:29:15 -0400

    first commit

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,33 +1,33 @@
-class EmailProcessor
-  def initialize(email)
-    @email = email
-  end
-
-  def process
-
-    # just to see if it reacts
-    Letter.create(title: "Something happened",  sender_id: 2, content:
-    "<h1>An email hit the server</h1>")
-
-    Letter.create!({ content: @email.body, sender: @email.from })
-
-  end
-end
-
-
-
-
-
-#
-#
 # class EmailProcessor
-#
-#   def self.process(email)
-#     Letter.create(title: "Be cool!",  sender_id: 2, content:
-#     "<h1>An email hit the server</h1>")
-#
-#     Letter.create!({ content: email.body, sender: email.from })
-#
+#   def initialize(email)
+#     @email = email
 #   end
 #
+#   def process
+#
+#     # just to see if it reacts
+#     Letter.create(title: "Something happened",  sender_id: 2, content:
+#     "<h1>An email hit the server</h1>")
+#
+#     Letter.create!({ content: @email.body, sender: @email.from })
+#
+#   end
 # end
+#
+
+
+
+
+
+
+class EmailProcessor
+
+  def self.process(email)
+    Letter.create(title: "Be cool!",  sender_id: 2, content:
+    "<h1>An email hit the server</h1>")
+
+    Letter.create!({ content: email.body, sender: email.from })
+
+  end
+
+end
