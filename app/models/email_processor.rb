77f7106a5725f8532c6e652b54b7a128b9444bf0
Kyle Hogan <kylehogan@Kyles-Air.home>
2015-03-12 18:12:57 -0400

    close

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -23,9 +23,8 @@
 class EmailProcessor
 
   def self.process(email)
-
-    Letter.create(title: "Be cool!",  sender_id: 2, content:
-    "<h1>An email hit the server</h1>")
+    Letter.create(title: "Be cool!",  sender_id: 1, content:
+    "Now testing with id 1")
 
     Letter.create!({ content: email.body, sender: email.from })
 
