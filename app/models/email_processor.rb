faeefbed4d7a2d18caa603994c0ed4d8b4891c60
Kyle Hogan <kylehogan@Kyles-MacBook-Air.local>
2015-03-13 16:56:53 -0400

    testing sender create

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -8,10 +8,12 @@ class EmailProcessor
     # just to see if it reacts
     # Letter.create(title: "Something happened",  sender_id: 1, content:
     # "<h1>An email hit the server</h1>")
-    # @sender = Sender.find_by(name: @email.from.token , host: @email.from.host)
-    #   if @sender == nil
-    #     @sender = Sender.create({name: @email.from.token , host: @email.from.host})
-    #   end
+
+    # trying to create sender info in a table
+    @sender = Sender.find_by(name: @email.from[:token] , host: @email.from[:host])
+      if @sender == nil
+        @sender = Sender.create({name: @email.from[:token] , host: @email.from[:host]})
+      end
     Letter.create!({ content: "#{@email.body}#{@email.from[:token]}#{@email.from[:host]}", sender_id: 2, title: @email.subject })
 
 
